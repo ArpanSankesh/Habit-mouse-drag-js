204a08d3288c5058e43fccd0f6a137623c2bd319
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-13 22:52:09 +0530

    Background revel

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let main = document.querySelector("#main");
+let afterImg = document.querySelector(".after-background");
+let arrows = document.querySelector(".arrows");
 
-let move = () => {
-    
+let move = (delts) => {
+    afterImg.style.width = delts.clientX + 'px';
+    arrows.style.top = delts.clientY + 'px' ;
+    arrows.style.left = delts.clientX  + 'px' + -25 + 'px';
+    console.log(delts.clientX);
 }
-main.addEventListener('mousemove', move)
+main.addEventListener('mousemove', move)    
