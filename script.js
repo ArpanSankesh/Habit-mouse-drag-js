9d1c6cf6da36f55791fb1b2ded50613151f693f9
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-13 23:55:30 +0530

    update

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let afterImg = document.querySelector(".after-background");
 let arrows = document.querySelector(".arrows");
 
 let move = (delts) => {
-    afterImg.style.width = delts.clientX + 'px';
-    // arrows.style.top = delts.clientY + 'px' ;
-    // arrows.style.left = delts.clientX  + 'px' + -25 + 'px';
-}
-mainBg.addEventListener('mousemove', move)    
+  afterImg.style.width = delts.clientX + "px";
+  // arrows.style.top = delts.clientY + 'px' ;
+  // arrows.style.left = delts.clientX  + 'px' + -25 + 'px';
+};
+mainBg.addEventListener("mousemove", move);
